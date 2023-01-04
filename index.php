c252f57af7b9a69a3d5e66a17f433bd75dc5944c
Ondrej-reinelt <121171379+Ondrej-reinelt@users.noreply.github.com>
2023-01-04 10:13:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-čau
+<?php
+
+function selection_sort($numbers) {
+    for ($i = 0; $i < count($numbers); $i++) {
+      $min_c = $i;
+      for ($j = $i + 1; $j < count($numbers); $j++) {
+        if ($numbers[$j] < $numbers[$min_c]) {
+          $min_c = $j;
+        }
+      }
+      if ($min_c != $i) {
+        $temp = $numbers[$i];
+        $numbers[$i] = $numbers[$min_c];
+        $numbers[$min_c] = $temp;
+      }
+    }
+    return $numbers;
+   
+  }
+  
+  $numbers = [8, -3, 9, 20, 15, 90, 12];
+  print_r(selection_sort($numbers));
+?>
