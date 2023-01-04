d7bad8f7599b8c3b57326d059a6edb7dfb04f82e
Ondrej-reinelt <121171379+Ondrej-reinelt@users.noreply.github.com>
2023-01-04 14:13:30 +0100

    Update index.php

--- index.php
+++ index.php
@@ -20,4 +20,29 @@ function selection_sort($numbers) {
   
   $numbers = [8, -3, 9, 20, 15, 90, 12];
   print_r(selection_sort($numbers));
+
+
+  //bubble sort
+
+  function bubble_sort($numbers) {
+    while (true) {
+      $swapped = false;
+      for ($i = 0; $i < count($numbers) - 1; $i++) {
+        if ($numbers[$i] > $numbers[$i + 1]) {
+          $temp = $numbers[$i];
+          $numbers[$i] = $numbers[$i + 1];
+          $numbers[$i + 1] = $temp;
+          $swapped = true;
+        }
+      }
+      if (!$swapped) {
+        break;
+      }
+    }
+  }
+  
+  $numbers = [30, 70, 42, 91, 53, 2, 16, 18];
+  bubble_sort($numbers);
+  print_r($numbers);
+  
 ?>
